095c7429839eae98faf962c95fe60c5b720a4155
marisua <marisua@yahoo.com>
2022-01-27 23:44:00 -0600

    anaconda

--- testing.py
+++ testing.py
@@ -16,19 +16,26 @@ print(insurance_cost)
 rows = []
 for row in csvreader:
     rows.append(row)
-    #print(row)
-    #print(row[0])
-    if row[1]=="female":
-        row[1]=0
-    else:
-        row[1]=1
-    
-    if row[4]=="yes":
-        row[4]= 1
-    else:
-        row[4]=0
-    
-    insurance_cost=250*int(row[0])+128*int(row[1])*(-1) + 370*float(row[2])+425*int(row[3])+24000*int(row[4])-12500
-    print("Insurance cost = %.2f \n " % insurance_cost)
-#print(rows)
+   
+print(rows)
 file.close()
+
+
+
+
+
+
+ #print(row)
+    #print(row[0])
+#     if row[1]=="female":
+#         row[1]=0
+#     else:
+#         row[1]=1
+    
+#     if row[4]=="yes":
+#         row[4]= 1
+#     else:
+#         row[4]=0
+    
+#     insurance_cost=250*int(row[0])+128*int(row[1])*(-1) + 370*float(row[2])+425*int(row[3])+24000*int(row[4])-12500
+#     print("Insurance cost = %.2f \n " % insurance_cost)
